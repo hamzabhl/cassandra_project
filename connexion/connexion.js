19c6d4a506fda9cea7b35f044105d301fe9aba15
Hamza Bahloul <bhl531hamza@gmail.com>
2026-04-20 00:21:21 +0100

    final version

--- connexion/connexion.js
+++ connexion/connexion.js
@@ -1,13 +1,14 @@
+require('dotenv').config();
 const cassandra = require('cassandra-driver');
 
-// Create client
+// Read values from .env
 const client = new cassandra.Client({
-    contactPoints: ['127.0.0.1'], // your Cassandra node
-    localDataCenter: 'datacenter1', // default for local setup
-    keyspace: 'ecommerce' // your DB name
+    contactPoints: process.env.CASSANDRA_CONTACT_POINTS.split(','),
+    localDataCenter: process.env.CASSANDRA_DATACENTER,
+    keyspace: process.env.CASSANDRA_KEYSPACE
 });
 
-// Connect function (optional)
+// Connect function
 async function connectDB() {
     try {
         await client.connect();
